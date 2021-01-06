15493632f3e764c88ea3bbea58ef75f9c7dffbd2
Jurij Waite <jurijlwaite@gmail.com>
2021-01-05 18:37:26 -0800

    Added day2 solution for part 2.

--- day2/day2.py
+++ day2/day2.py
@@ -1,16 +1,23 @@
 with open('input2.txt') as f:
 
-    valid_pw_count = 0
+    valid_pw_count_part1 = 0
+    valid_pw_count_part2 = 0
     for line in f.readlines():
         # Parse input text
         frequencies, letter, password = line.split()
         lower, upper = map(int, frequencies.split('-'))
         letter = letter[0]
 
-        # Check the password fits the criteria
+        # Check the password fits the criteria for part 1
         letter_frequency = password.count(letter)
         if lower <= letter_frequency <= upper:
-            valid_pw_count += 1
+            valid_pw_count_part1 += 1
 
-    print(valid_pw_count)
+        # Check the password fits the criteria for part 2
+        if (password[lower - 1] == letter and password[upper - 1] !=
+                letter) or (password[lower - 1] != letter and password[upper - 1] == letter):
+            valid_pw_count_part2 += 1
+
+    print(valid_pw_count_part1)
+    print(valid_pw_count_part2)
     f.close()
