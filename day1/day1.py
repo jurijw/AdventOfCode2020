f2fabda0d15bbedcaea2e47240ed54bd543a29a3
Jurij Waite <jurijlwaite@gmail.com>
2021-01-05 17:41:29 -0800

    Added solution for day1 part 1.

--- day1/day1.py
+++ day1/day1.py
@@ -5,5 +5,12 @@ with open('input1.txt') as f:
     assert len(set(nums)) == len(nums), "Inputs are not unique"
     f.close()
 
-for num in nums:
-    pass
+
+def sol1():
+    for num1 in nums:
+        for num2 in nums:
+            if num1 + num2 == 2020:
+                return num1 * num2
+
+
+print(sol1())
